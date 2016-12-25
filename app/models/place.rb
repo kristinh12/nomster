3bd29c702ff630c1f035592d3a91d7da638a93f3
Kristin Heininger <dauphindouze@comcast.net>
2016-12-25 05:10:41 +0000

    Added latitude and longitude

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
   validates :name, :address, :description, presence: true
   validates :name, length: { minimum: 4 }
 end 
